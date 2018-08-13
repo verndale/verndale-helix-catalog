c933a6360554a70c27fd3ff65e1c63c9c3596cfa
Steven Striga <steven.striga@verndale.com>
2018-08-13 12:49:25 -0400

    Item fallback tools verified working in sitecore 9 update 1

--- Verndale.Feature.LanguageFallback/Commands/MultilingualTemplateCommand.cs
+++ Verndale.Feature.LanguageFallback/Commands/MultilingualTemplateCommand.cs
@@ -1,251 +1,259 @@
-﻿using System;
-using System.Linq;
-using Sitecore;
+﻿using Sitecore;
 using Sitecore.Data;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Shell.Framework.Commands;
 using Sitecore.Web.UI.Sheer;
+using System;
+using System.Linq;
 
 namespace Verndale.Feature.LanguageFallback.Commands
 {
-	/// <summary>
-	/// Base class for processing custom multilingual commands for Sitecore templates.
-	/// </summary>
-	public abstract class MultilingualTemplateCommand : Command
-	{
-		#region instance variables
+    /// <summary>
+    /// Base class for processing custom multilingual commands for Sitecore templates.
+    /// </summary>
+    [Serializable]
+    public abstract class MultilingualTemplateCommand : Command
+    {
+        #region instance variables
 
-		/// <summary>
-		/// Gets/sets the item selected by the user.
-		/// </summary>
-		private Item _selectedItem;
+        /// <summary>
+        /// Gets/sets the item selected by the user.
+        /// </summary>
+        private Item _selectedItem;
 
-		#endregion
+        #endregion
 
-		#region Abstract methods
+        #region Abstract methods
 
-		/// <summary>
-		/// Gets the name of the command.
-		/// </summary>
-		public abstract string CommandName { get; }
+        /// <summary>
+        /// Gets the name of the command.
+        /// </summary>
+        public abstract string CommandName { get; }
 
-		/// <summary>
-		/// Process the command with the given item.
-		/// </summary>
-		public abstract int Process(Item parentItem);
+        /// <summary>
+        /// Process the command with the given item.
+        /// </summary>
+        public abstract int Process(Item contextItem);
 
-		#endregion
+        #endregion
 
-		#region Sitecore UI Methods
+        #region Sitecore UI Methods
 
-		/// <summary>
-		/// Entry point for the Sitecore command.
-		/// </summary>
-		public override void Execute(CommandContext context)
-		{
-			Assert.IsNotNull("context", "Context cannot be null.");
+        /// <summary>
+        /// Entry point for the Sitecore command.
+        /// </summary>
+        public override void Execute(CommandContext context)
+        {
+            Assert.IsNotNull("context", "Context cannot be null.");
 
-			// Store the Sitecore item the user selected before they clicked the command button.
-			_selectedItem = context.Items.FirstOrDefault();
+            // Store the Sitecore item the user selected before they clicked the command button.
+            _selectedItem = context.Items.FirstOrDefault();
 
-			// Validate the selected item.
-			string message = string.Empty;
-			if (ValidateInput(_selectedItem, out message))
-			{
-				// Item was validated, show the confirm modal.
-				Context.ClientPage.Start(this, "Confirm", context.Parameters);
-			}
-			else
-			{
-				// Sitecore item failed validation, show alert message.
-				ShowAlert(message);
-			}
-		}
+            // Validate the selected item.
+            string message = string.Empty;
+            if (ValidateInput(_selectedItem, out message))
+            {
+                // Item was validated, show the confirm modal.
+                Context.ClientPage.Start(this, "Confirm", context.Parameters);
+            }
+            else
+            {
+                // Sitecore item failed validation, show alert message.
+                ShowAlert(message);
+            }           
+        }
 
-		/// <summary>
-		/// Sitecore UI method that confirms to process
-		/// </summary>
-		public void Confirm(ClientPipelineArgs args)
-		{
-			if (!args.IsPostBack)
-			{
-				// Prompt the user to confirm they want to perform the action.
-				SheerResponse.Confirm(
-					string.Format("Execute \"{0}\" on\r\n\r\n{1}\r\n\r\nand all descendants?",
-						CommandName,
-						_selectedItem.Paths.FullPath
-					)
-				);
+        /// <summary>
+        /// Sitecore UI method that confirms to process
+        /// </summary>
+        public void Confirm(ClientPipelineArgs args)
+        {
+            if (!args.IsPostBack)
+            {
+                // Prompt the user to confirm they want to perform the action.
+                SheerResponse.Confirm(
+                    string.Format("Execute \"{0}\" on\r\n\r\n{1}\r\n\r\nand all descendants?", 
+                        CommandName, 
+                        _selectedItem.Paths.FullPath
+                    )
+                );
 
-				// Pause the UI to force user to respond to the confirmation modal.
-				args.WaitForPostBack();
-			}
-			else
-			{
-				// Current request is a post back 
-				if (!args.Result.Equals("yes", StringComparison.InvariantCultureIgnoreCase))
-				{
-					// User did not click OK / Yes, so do nothing.
-					return;
-				}
+                // Pause the UI to force user to respond to the confirmation modal.
+                args.WaitForPostBack();
+            }
+            else
+            {
+                // Current request is a post back 
+                if (!args.Result.Equals("yes", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    // User did not click OK / Yes, so do nothing.
+                    return;
+                }
 
-				// Validate the user selection.
-				string message = string.Empty;
-				if (!ValidateInput(_selectedItem, out message))
-				{
-					// Selected sitecore item was not valid, show an alert.
-					ShowAlert(message);
-				}
-				else
-				{
-					// Trigger the process method in the implementing / concrete command class.
-					int changeCount = Process(_selectedItem);
+                // Validate the user selection.
+                string message = string.Empty;
+                if (!ValidateInput(_selectedItem, out message))
+                {
+                    // Selected sitecore item was not valid, show an alert.
+                    ShowAlert(message);
+                }
+                else
+                {
+                    // Trigger the process method in the implementing / concrete command class.
+                    int changeCount = Process(_selectedItem);
 
-					// Notify the user in the UI that the processing has completed.
-					ShowAlert(BuildCompletionMessage(changeCount));
-				}
-			}
-		}
+                    // Notify the user in the UI that the processing has completed.
+                    ShowAlert(BuildCompletionMessage(changeCount));
+                }
+            }
+        }
 
-		#endregion
+        #endregion
 
-		#region Virtual, Non Abstract Methods
+        #region Virtual, Non Abstract Methods
 
-		/// <summary>
-		/// Validates the selected Item.
-		/// </summary>
-		protected virtual bool ValidateInput(Item item, out string message)
-		{
-			// Validate that the user is logged in.
-			if (!Context.User.IsAuthenticated)
-			{
-				message = "User is not authenticated.";
+        /// <summary>
+        /// Validates the selected Item.
+        /// </summary>
+        protected virtual bool ValidateInput(Item item, out string message)
+        {
+            // Validate that the user is logged in.
+            if (!Context.User.IsAuthenticated)
+            {
+                message = "User is not authenticated.";
 
-				return false;
-			}
+                return false;
+            }
 
-			// Ensure an item was selected.
-			if (item == null)
-			{
-				message = "Please select an item.";
+            // Ensure an item was selected.
+            if (item == null)
+            {
+                message = "Please select an item.";
 
-				return false;
-			}
+                return false;
+            }
 
-			// Make sure the selected item is a template item (i.e. beneath /sitecore/templates).
-			if (!item.Paths.FullPath.StartsWith("/sitecore/templates", StringComparison.InvariantCultureIgnoreCase))
-			{
-				message = "Please select an item beneath /sitecore/templates";
+            // Make sure the selected item is a template item (i.e. beneath /sitecore/templates).
+            if (!item.Paths.FullPath.StartsWith("/sitecore/templates", StringComparison.InvariantCultureIgnoreCase))
+            {
+                message = "Please select an item beneath /sitecore/templates";
 
-				return false;
-			}
+                return false;
+            }
 
-			// Successfully validated.
-			message = string.Empty;
-			return true;
-		}
+            // Successfully validated.
+            message = string.Empty;
+            return true;
+        }
 
-		/// <summary>
-		/// Shows an alert Modal in the Sitecore UI.
-		/// </summary>
-		protected virtual void ShowAlert(string message)
-		{
-			if (!string.IsNullOrWhiteSpace(message))
-			{
-				Context.ClientPage.ClientResponse.Alert(message);
-			}
-		}
+        /// <summary>
+        /// Shows an alert Modal in the Sitecore UI.
+        /// </summary>
+        protected virtual void ShowAlert(string message)
+        {
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Context.ClientPage.ClientResponse.Alert(message);
+            }
+        }
 
-		/// <summary>
-		/// Sets a Sitecore checkbox field value.
-		/// </summary>
-		/// <returns>True if the checkbox value CHANGED, otherwise false.</returns>
-		protected virtual bool SetCheckboxFieldValue(Item item, ID fieldId, bool newCheckedValue = true)
-		{
-			bool valueChanged = false;
+        /// <summary>
+        /// Sets a Sitecore checkbox field value.
+        /// </summary>
+        /// <returns>True if the checkbox value CHANGED, otherwise false.</returns>
+        protected virtual bool SetCheckboxFieldValue(Item item, ID fieldId, bool newCheckedValue = true)
+        {
+            bool valueChanged = false;
 
-			if (item == null)
-			{
-				Log.Warn("Verndale.Feature.LanguageFallback.Commands.EnableLanguageFallbackOnTemplateFields.SetFieldValue(): item is null, skipping...", this);
+            if (item == null)
+            {
+                Log.Warn("LanguageFallbackTools.Fallback.EnableLanguageFallbackOnTemplateFields.SetFieldValue(): item is null, skipping...", this);
 
-				return false;
-			}
+                return false;
+            }
+            
+            CheckboxField field = item.Fields[fieldId];
 
-			CheckboxField field = item.Fields[fieldId];
+            if (field == null)
+            {
+                // Log that the field is missing on the item.
+                Log.Warn(string.Format("LanguageFallbackTools.Fallback.EnableLanguageFallbackOnTemplateFields.SetFieldValue(): Item {0} field '{1}' is null. skipping...",
+                    item.Paths.FullPath,
+                    fieldId),
+                this);
 
-			if (field == null)
-			{
-				// Log that the field is missing on the item.
-				Log.Warn(
-					$"Verndale.Feature.LanguageFallback.Commands.EnableLanguageFallbackOnTemplateFields.SetFieldValue(): Item {item.Paths.FullPath} field '{fieldId}' is null. skipping...",
-				this);
+                return false;
+            }
+                    
+            // Track if the value changed.
+            valueChanged = (field.Checked != newCheckedValue);
 
-				return false;
-			}
+            // Log the changes.
+            Log.Info(
+                string.Format("LanguageFallbackTools.Fallback.EnableLanguageFallbackOnTemplateFields.SetFieldValue(): Item {0}: Old value: '{1}'; New value: '{2}'; valueChanged: {3}",
+                    item.Paths.FullPath,
+                    field.Checked,
+                    newCheckedValue,
+                    valueChanged),
+                this
+            );
 
-			// Track if the value changed.
-			valueChanged = (field.Checked != newCheckedValue);
+            // Only bother to trigger edit mode on the item if we are going to make an actual update!
+            if (valueChanged)
+            {
+                // Disable item security to avoid random security errors. 
+                // If the user has access to the button to trigger this command, thats enough of a security check.
 
-			// Log the changes.
-			Log.Info(
-				$"Verndale.Feature.LanguageFallback.Commands.EnableLanguageFallbackOnTemplateFields.SetFieldValue(): Item {item.Paths.FullPath}: Old value: '{field.Checked}'; New value: '{newCheckedValue}'; valueChanged: {valueChanged}",
-				this
-			);
+                using (new Sitecore.SecurityModel.SecurityDisabler())
+                {
+                    try
+                    {
+                        // update the value.
+                        item.Editing.BeginEdit();
 
-			// Only bother to trigger edit mode on the item if we are going to make an actual update!
-			if (valueChanged)
-			{
-				// Disable item security to avoid random security errors. 
-				// If the user has access to the button to trigger this command, thats enough of a security check.
+                        // update the field value
+                        field.Checked = newCheckedValue;
+                    }
+                    catch (Exception ex)
+                    {
+                        // Log error to sitecore log.
+                        Log.Error(string.Format("LanguageFallbackTools.Fallback.EnableLanguageFallbackOnTemplateFields.SetFieldValue(): Item {0} ERROR: '{1}'",
+                                item.Paths.FullPath,
+                                ex.Message),
+                            ex,
+                        this);
+                    }
+                    finally
+                    {
+                        // Ensure that even if there is an error that end edit editing mode on the item.
+                        item.Editing.EndEdit();
+                    }
+                
+                }
+            }
 
-				using (new Sitecore.SecurityModel.SecurityDisabler())
-				{
-					try
-					{
-						// update the value.
-						item.Editing.BeginEdit();
+            return valueChanged;
+        }
 
-						// update the field value
-						field.Checked = newCheckedValue;
-					}
-					catch (Exception ex)
-					{
-						// Log error to sitecore log.
-						Log.Error($"Verndale.Feature.LanguageFallback.Commands.EnableLanguageFallbackOnTemplateFields.SetFieldValue(): Item {item.Paths.FullPath} ERROR: '{ex.Message}'",
-							ex,
-						this);
-					}
-					finally
-					{
-						// Ensure that even if there is an error that end edit editing mode on the item.
-						item.Editing.EndEdit();
-					}
+        #endregion
 
-				}
-			}
+        #region private methods
 
-			return valueChanged;
-		}
+        /// <summary>
+        /// Builds the completion message.
+        /// </summary>
+        private string BuildCompletionMessage(int changeCount)
+        {
+            if (changeCount == 1)
+            {
+                return "Processing completed. 1 item was updated.";
+            }
 
-		#endregion
+            return string.Format("Processing completed. {0:N0} items were updated.", changeCount);
+        }
 
-		#region private methods
-
-		/// <summary>
-		/// Builds the completion message.
-		/// </summary>
-		private string BuildCompletionMessage(int changeCount)
-		{
-			if (changeCount == 1)
-			{
-				return "Processing completed. One (1) item was updated.";
-			}
-
-			return $"Processing completed. {changeCount:N0} items were updated.";
-		}
-
-		#endregion
-	}
+        #endregion
+    }
 }
